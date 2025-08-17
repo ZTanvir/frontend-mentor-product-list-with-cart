9c8aa4b4101a5f3032df8938c73a2e5b193d6440
zahirul <zahirulopel@gmail.com>
2025-08-17 10:23:13 +0600

    product context,product context provider, use product created

---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+export const ProductContext = createContext([]);
+
+export const ProductContextProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/products");
+        if (!response.ok) {
+          throw new Error("Error on fetching products data");
+        }
+        const data = await response.json();
+        console.log(data);
+
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <ProductContext.Provider value={{ products, error, isLoading }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export const useProducts = () => {
+  return useContext(ProductContext);
+};
