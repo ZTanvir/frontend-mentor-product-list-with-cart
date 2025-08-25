acef2e6d9821dfce16dfbe750e137634d40d222a
zahirul <zahirulopel@gmail.com>
2025-08-25 11:59:38 +0600

    covert cart action function useState to useReducer , so code is more readable and debug made eassy

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,42 +1,72 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useReducer, useState } from "react";
 
 const cartContext = createContext([]);
 
-export function CartProvider({ children }) {
-  const [cart, setCart] = useState([]);
-  const addItemToCart = (product) => {
-    const copyProduct = { ...product };
-    copyProduct.qty = 1;
-    setCart((prev) => prev.concat(copyProduct));
-  };
-  const removeItemFromCart = (name) => {
-    const nextCart = cart.filter((item) => !(item.name === name));
-    setCart(nextCart);
-  };
-  const incrementProductAmount = (product) => {
-    const nextCart = cart.map((item) => {
-      if (item.name === product.name) {
-        return { ...item, qty: item.qty + 1 };
-      }
-      return { ...item };
-    });
-    setCart(nextCart);
-  };
-  const decrementProductAmount = (product) => {
-    const cartProduct = cart.find((item) => item.name === product.name);
+function cartReducer(cart, action) {
+  switch (action.type) {
+    case "added": {
+      const copyProduct = { ...action.product };
+      copyProduct.qty = 1;
+      const nextCart = cart.concat(copyProduct);
+      return nextCart;
+    }
+    case "removed": {
+      const nextCart = cart.filter(
+        (item) => !(item.name === action.productName)
+      );
+      return nextCart;
+    }
 
-    if (cartProduct.qty >= 2) {
+    case "increment_product_quantity": {
       const nextCart = cart.map((item) => {
-        if (item.name === cartProduct.name) {
-          return { ...item, qty: item.qty - 1 };
+        if (item.name === action.product.name) {
+          return { ...item, qty: item.qty + 1 };
         }
         return { ...item };
       });
-      setCart(nextCart);
-    } else if (cartProduct.qty <= 1) {
-      const nextCart = cart.filter((item) => item.name !== cartProduct.name);
-      setCart(nextCart);
+      return nextCart;
     }
+    case "decrement_product_quantity": {
+      const cartProduct = cart.find(
+        (item) => item.name === action.product.name
+      );
+
+      if (cartProduct.qty >= 2) {
+        const nextCart = cart.map((item) => {
+          if (item.name === cartProduct.name) {
+            return { ...item, qty: item.qty - 1 };
+          }
+          return { ...item };
+        });
+        return nextCart;
+      } else if (cartProduct.qty <= 1) {
+        const nextCart = cart.filter((item) => item.name !== cartProduct.name);
+        return nextCart;
+      }
+    }
+    default: {
+      throw Error("Unknown action: " + action.type);
+    }
+  }
+}
+
+export function CartProvider({ children }) {
+  const [cart, dispatch] = useReducer(cartReducer, []);
+
+  const addItemToCart = (product) => {
+    dispatch({ type: "added", product });
+  };
+
+  const removeItemFromCart = (name) => {
+    dispatch({ type: "removed", productName: name });
+  };
+
+  const incrementProductAmount = (product) => {
+    dispatch({ type: "increment_product_quantity", product });
+  };
+
+  const decrementProductAmount = (product) => {
+    dispatch({ type: "decrement_product_quantity", product });
   };
 
   return (
